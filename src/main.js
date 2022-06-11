39e456bbbe91617c9a4dd913f502f6e2a5b39aeb
Brenden Seidel <brenden.seidel@oliveai.com>
2022-06-10 20:29:26 -0400

    init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
+import BaseIcon from "@/components/BaseIcon.vue";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
+Vue.component("BaseIcon", BaseIcon);
 
 Vue.config.productionTip = false;
 
---
+++ src/views/EventsList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Events Listing</h1>
+
+    <EventCard />
+
+    <BaseIcon />
+  </div>
+</template>
+
+<script>
+import BaseIcon from "@/components/BaseIcon.vue";
+import EventCard from "@/components/EventCard.vue";
+export default {
+  components: { EventCard, BaseIcon },
+};
+</script>
+
+<style></style>
