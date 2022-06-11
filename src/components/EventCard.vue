f9567445f79e3d91d435110ff03564b2de933d78
Brenden Seidel <brenden.seidel@oliveai.com>
2022-06-10 22:08:44 -0400

    api calls with axios

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,7 +1,7 @@
 <template>
   <router-link
     class="event-link"
-    :to="{ name: 'event-show', params: { id: '1' } }"
+    :to="{ name: 'event-show', params: { id: event.id } }"
   >
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
@@ -12,23 +12,12 @@
 </template>
 
 <script>
-import BaseIcon from "./BaseIcon.vue";
 export default {
-  data() {
-    return {
-      event: {
-        id: 1,
-        title: "Beach Cleanup",
-        date: "Tue May 15 2018",
-        time: "6:00",
-        attendees: [
-          { id: "abc123", name: "Adam Jahr" },
-          { id: "def456", name: "Gregg Pollack" },
-        ],
-      },
-    };
+  props: {
+    event: {
+      type: Object,
+    },
   },
-  components: { BaseIcon },
 };
 </script>
 
